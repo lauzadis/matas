9436880151c62484afc599e208b2d356b05fb120
Matas Lauzadis <matas.lauzadis@gmail.com>
2024-01-29 00:46:20 -0500

    Quartz sync: Jan 29, 2024, 12:46 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 Matas",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "lauzadis.github.io/quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
